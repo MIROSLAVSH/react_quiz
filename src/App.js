8a36528fa15e9b723e5248ed4f4a19f18e9fd21e
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2023-02-06 12:23:28 +0300

    docs: react_quiz complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
+
+const questions = [
+  {
+    title: 'React - it is ... ?',
+    variants: ['library', 'framework', 'application'],
+    correct: 0,
+  },
+  {
+    title: 'Component - it is ... ',
+    variants: ['application', 'part of an application or page', 'that I dont know what is it'],
+    correct: 1,
+  },
+  {
+    title: 'What is JSX?',
+    variants: [
+      'This is just HTML',
+      'This is function',
+      'This is the same HTML, but with opportunity perform JS-code',
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="flaticon"/>
+      <h2>You guessed { correct } answers out of {questions.length}</h2>
+		<a href="/"><button>Try again</button></a>
+      
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+	const percentage = Math.round(step / questions.length * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (<li onClick={() => onClickVariant(index)} key={text}>{text}</li>))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
+	const [step, setStep] = React.useState(0);
+	const [correct, setCorrect] = React.useState(0);
+	const question = questions[step];
+
+	const onClickVariant = (index) => {
+		console.log(step, index);
+		setStep(step + 1)
+
+		if(index === question.correct){
+			setCorrect(correct + 1);
+		}
+	}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+			step !== questions.length ? (<Game step={step} question={question} onClickVariant={onClickVariant}/>) : 
+			(<Result correct={correct} />) 
+		}
     </div>
   );
 }
